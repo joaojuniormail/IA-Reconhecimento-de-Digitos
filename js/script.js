52de22e0f608bf65d91aeae4123c1c6cca958413
João B. S. Júnior <joaojunior.mail@gmail.com>
2015-11-29 17:56:05 -0300

    -Ajuste Responsivo

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-var teste = null;
 var NeuralNetwork = function () {
         var self = this,
             representationColumns = 5,
@@ -15,12 +14,12 @@ var NeuralNetwork = function () {
             drawElement = $('#draw-digit');
             representationElement = $('#neural-representation');
             resultElement = $('#result');
-            drawView = ENCOG.GUI.Drawing.create('draw-digit', Math.ceil(drawElement.width()), Math.ceil(drawElement.width()));
-            representationView = ENCOG.GUI.CellGrid.create('neural-representation', representationColumns, representationRows, Math.ceil(representationElement.width()), Math.ceil((representationElement.width() * 1.1)));
-            teste = drawView;
+            drawView = ENCOG.GUI.Drawing.create('draw-digit', Math.ceil(drawElement.width()),
+            Math.ceil(drawElement.width()));
+            representationView = ENCOG.GUI.CellGrid.create('neural-representation', representationColumns,
+            representationRows, Math.floor(representationElement.width()), Math.floor((representationElement.width() * 1.1)));
             representationView.outline = true;
             representationView.mouseDown = function (x, y) {};
-
             representationView.determineColor = function (row, col) {
                 var index = (row * this.gridWidth) + col;
                 if (representationData[index] < 0) {
@@ -33,7 +32,8 @@ var NeuralNetwork = function () {
             window.addEventListener('resize', function () {
                 clearTimeout();
                 setTimeout(function () {
-
+                    drawView.canvas.width = Math.ceil(drawElement.width());
+                    drawView.canvas.height = Math.ceil(drawElement.width());
                 }, 300);
             }, false);
 
@@ -48,6 +48,7 @@ var NeuralNetwork = function () {
         self.cleanView = function (ev) {
             drawView.clear();
             self.clearRepresentation();
+            resultElement.html("&nbsp;");
         };
 
         self.showRepresentationCurrent = function () {
@@ -72,23 +73,12 @@ var NeuralNetwork = function () {
 
             for (var c in charData) {
                 var data = charData[c];
-
-                // Now we will actually recognize the letter drawn.
-                // To do this, we will use a Euclidean distance
-                // http://www.heatonresearch.com/wiki/Euclidean_Distance
-
                 var sum = 0;
                 for (var i = 0; i < data.length; i++) {
                     var delta = data[i] - representationData[i];
                     sum = sum + (delta * delta);
                 }
-
                 sum = Math.sqrt(sum);
-
-                // Basically we are calculating the Euclidean distance between
-                // what was just drawn, and each of the samples we taught
-                // the program.  The smallest Euclidean distance is the char.
-
                 if (sum < bestScore || bestChar == '??') {
                     bestScore = sum;
                     bestChar = c;
@@ -96,32 +86,99 @@ var NeuralNetwork = function () {
 
             }
             resultElement.text(bestChar);
-            console.log(bestChar);
-
-            /*            drawView.clear();
-                        self.clearRepresentation();*/
         };
 
         var learning = function () {
-            self.createDigit("0", new Array(-1, 1, 1, 1, -1, 1, 1, -1, 1, 1, 1, -1, -1, -1, 1, 1, -1, -1, -1, 1, 1, -1, -1, -1, 1, 1, -1, -1, -1, 1, 1, 1, -1, -1, 1, -1, 1, 1, 1, -1));
-            self.createDigit("1", new Array(-1, -1, -1, 1, -1, -1, -1, 1, -1, -1,
-                1, 1, 1, -1, -1,
-                1, 1, 1, 1, -1, -1, 1, 1, 1, 1, -1, -1, -1, 1, 1, -1, -1, -1, 1, 1, -1, 1, 1, 1, 1));
+            self.createDigit("0", new Array(
+                -1,  1,  1,  1, -1,
+                 1,  1, -1,  1,  1,
+                 1, -1, -1, -1,  1,
+                 1, -1, -1, -1,  1,
+                 1, -1, -1, -1,  1,
+                 1, -1, -1, -1,  1,
+                 1,  1, -1, -1,  1, 
+                -1,  1,  1,  1, -1));
+            self.createDigit("1", new Array(
+                -1, -1,  1,  1, -1,
+                -1,  1,  1,  1, -1,
+                 1,  1,  1,  1, -1,
+                 1, -1,  1,  1, -1,
+                -1, -1,  1,  1, -1,
+                -1, -1,  1,  1, -1,
+                -1, -1,  1,  1, -1,
+                 1,  1,  1,  1,  1));
             self.createDigit("2", new Array(
-                1, 1, 1, -1, -1, -1, -1, 1, 1, -1, -1, -1, -1, 1, -1, -1, -1, -1, 1, -1, -1, -1, -1, 1, -1, 1, 1, 1, 1, -1, 1, -1, 1, 1, -1, 1, 1, 1, 1, 1));
+                 1,  1,  1, -1, -1,
+                -1, -1,  1,  1, -1,
+                -1, -1, -1,  1, -1,
+                -1, -1, -1,  1, -1,
+                -1, -1, -1,  1, -1,
+                 1,  1,  1,  1, -1,
+                 1, -1,  1,  1, -1,
+                 1,  1,  1,  1,  1));
             self.createDigit("3", new Array(
-                1, 1, 1, 1, -1, -1, -1, -1, 1, 1, -1, -1, -1, 1, 1, -1, -1, 1, 1, -1, -1, 1, 1, 1, 1, -1, -1, -1, -1, 1, -1, -1, -1, -1, 1, 1, 1, 1, 1, 1));
+                 1,  1,  1,  1, -1,
+                -1, -1, -1,  1,  1,
+                -1, -1, -1,  1,  1,
+                -1, -1,  1,  1, -1,
+                -1,  1,  1,  1,  1,
+                -1, -1, -1, -1,  1,
+                -1, -1, -1, -1,  1,
+                 1,  1,  1,  1,  1));
             self.createDigit("4", new Array(
-                1, -1, -1, 1, -1, 1, -1, -1, 1, -1, 1, -1, -1, 1, -1, 1, 1, 1, 1, 1, -1, -1, -1, 1, -1, -1, -1, -1, 1, -1, -1, -1, -1, 1, -1, -1, -1, -1, 1, -1));
+                 1,  1, -1, -1,  1,
+                 1,  1, -1, -1,  1,
+                 1, -1, -1, -1,  1,
+                 1,  1,  1,  1,  1,
+                -1, -1, -1,  1,  1,
+                -1, -1, -1,  1,  1,
+                -1, -1, -1,  1,  1,
+                -1, -1, -1,  1,  1));
             self.createDigit("5", new Array(
-                1, 1, 1, 1, 1, 1, -1, -1, -1, -1, 1, -1, -1, -1, -1, 1, 1, 1, 1, 1, -1, -1, -1, -1, 1, -1, -1, -1, -1, 1, -1, -1, -1, -1, 1, 1, 1, 1, 1, 1));
-            self.createDigit("6", new Array(-1, 1, 1, 1, -1, 1, 1, -1, -1, -1, 1, -1, -1, -1, -1, 1, -1, 1, 1, -1, 1, 1, 1, 1, 1, 1, 1, -1, -1, 1, 1, 1, -1, -1, 1, -1, 1, 1, 1, 1));
+                 1,  1,  1,  1,  1,
+                 1, -1, -1, -1, -1,
+                 1, -1, -1, -1, -1,
+                 1,  1,  1,  1,  1,
+                -1, -1, -1, -1,  1,
+                -1, -1, -1, -1,  1,
+                -1, -1, -1, -1,  1,
+                 1,  1,  1,  1,  1));
+            self.createDigit("6", new Array(
+                -1,  1,  1,  1, -1,
+                 1,  1, -1, -1, -1, 
+                 1, -1, -1, -1, -1, 
+                 1, -1,  1,  1, -1, 
+                 1,  1,  1,  1,  1, 
+                 1,  1, -1, -1,  1, 
+                 1,  1, -1, -1,  1, 
+                -1,  1,  1,  1,  1));
             self.createDigit("7", new Array(
-                1, 1, 1, 1, 1, -1, -1, -1, 1, 1, -1, -1, -1, 1, -1, -1, -1, -1, 1, -1, -1, -1, 1, 1, -1, -1, -1, 1, -1, -1, -1, 1, 1, -1, -1, -1, 1, -1, -1, -1));
+                 1,  1,  1,  1,  1, 
+                -1, -1, -1,  1,  1, 
+                -1, -1, -1,  1, -1, 
+                -1, -1, -1,  1, -1, 
+                -1, -1,  1,  1, -1, 
+                -1, -1,  1, -1, -1, 
+                -1,  1,  1, -1, -1, 
+                -1,  1, -1, -1, -1));
             self.createDigit("8", new Array(
-                1, 1, 1, 1, 1, 1, -1, -1, -1, 1, 1, -1, -1, -1, 1, 1, 1, 1, 1, 1, -1, 1, 1, 1, 1, 1, 1, -1, -1, 1, 1, -1, -1, -1, 1, 1, 1, 1, 1, 1));
+                 1,  1,  1,  1,  1, 
+                 1, -1, -1, -1,  1, 
+                 1, -1, -1, -1,  1, 
+                 1,  1,  1,  1,  1, 
+                -1,  1,  1,  1,  1, 
+                 1,  1, -1, -1,  1, 
+                 1, -1, -1, -1,  1, 
+                 1,  1,  1,  1,  1));
             self.createDigit("9", new Array(
-                1, 1, 1, 1, 1, 1, 1, -1, -1, 1, 1, -1, -1, -1, 1, 1, 1, 1, 1, 1, -1, -1, -1, -1, 1, -1, -1, -1, -1, 1, -1, -1, -1, -1, 1, -1, -1, -1, -1, 1));
+                 1,  1,  1,  1,  1, 
+                 1,  1, -1,  1,  1, 
+                 1, -1, -1, -1,  1, 
+                 1,  1,  1,  1,  1, 
+                -1, -1, -1, -1,  1, 
+                -1, -1, -1, -1,  1, 
+                -1, -1, -1, -1,  1, 
+                -1, -1, -1, -1,  1));
         };
     },
     app = new NeuralNetwork();
